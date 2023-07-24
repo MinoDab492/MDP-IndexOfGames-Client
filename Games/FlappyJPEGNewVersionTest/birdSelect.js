0edd31ab80ce36ec7351a6e40b17a95c95af005a
Heath Garvin <60587708+MinoDab492@users.noreply.github.com>
2023-07-23 23:54:14 -0700

    Update birdSelect.js
    Also breaking the CodeQL scanning, plus, this game will be removed anyways, so why keep this around? Will be properly removed tomorrow.

--- Games/FlappyJPEGNewVersionTest/birdSelect.js
+++ Games/FlappyJPEGNewVersionTest/birdSelect.js
@@ -1,10 +1,1 @@
-let N = 2 // no of images
-const imageBase='www.minodabproductions.cf/flappybird/';// relative or absolute url
-if(N === '2') {
-    document.querySelector(".img").src = `www.minodabproductions.cf/flappybird/bird2.png`
-    console.log('bird2.png has been chosen, I should really all a title screen to make it say either Flappy JPEG or Flappy PNG. That would be kind of funny')
-}
-elif(N === '1') {
-    document.querySelector(".img").src = `www.minodabproductions.cf/flappybird/bird1.jpg`
-    console.log("Nothing is better than the original, bird1.jpeg has been chosen.")
-}
+
