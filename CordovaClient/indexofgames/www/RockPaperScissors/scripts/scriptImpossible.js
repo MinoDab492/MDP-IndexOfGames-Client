3f5558a79e63da42837b353a610bc04e0dd98de0
MinoDab492 <60587708+MinoDab492@users.noreply.github.com>
2023-07-24 22:44:54 -0700

    We got Cordova running, maybe. This is for the ChromeOS client

---
+++ CordovaClient/indexofgames/www/RockPaperScissors/scripts/scriptImpossible.js
@@ -0,0 +1,36 @@
+function computerChoice() {
+    const choices = ['rock', 'paper', 'scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+  }
+  
+  function play(userChoice) {
+    computer = computerChoice();
+
+    if(userChoice === 'scissors') {
+        computer = 'rock'
+        }
+    if(userChoice === 'paper') {
+        computer = 'scissors'
+        }
+    if(userChoice === 'rock') {
+        computer = 'paper'
+        }
+  
+    if (
+      (userChoice === 'rock' && computer === 'paper') ||
+      (userChoice === 'paper' && computer === 'scissors') ||
+      (userChoice === 'scissors' && computer === 'rock')
+    ) {
+      setResult(`You lose! ${computer} beats ${userChoice}.`);
+    } else if (userChoice === computer) {
+      setResult('It\'s a tie!');
+    } else {
+      setResult(`You win! ${userChoice} beats ${computer}.`);
+    }
+  }
+  
+  function setResult(message) {
+    document.getElementById('result').innerText = message;
+  }
+  
