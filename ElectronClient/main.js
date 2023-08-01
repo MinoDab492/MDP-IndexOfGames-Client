3fbee51f1ed4014e16f5c35485f611b5252e0905
MinoDab492 <60587708+MinoDab492@users.noreply.github.com>
2023-08-01 11:02:43 -0700

    Updates the main IndexOfGames.html file and main.js to the right directory

--- ElectronClient/main.js
+++ ElectronClient/main.js
@@ -7,7 +7,7 @@ const createWindow = () => {
     height: 1080,
   });
 
-  win.loadFile('GamesUpdated/modified_IndexOfGames.html');
+  win.loadFile('Games/IndexOfGames.html');
 };
 
 app.whenReady().then(() => {
